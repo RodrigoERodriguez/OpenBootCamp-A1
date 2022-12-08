afaa25879f9b7607736151bc315325c38c391ebb
Torito <111804493+RodrigoERodriguez@users.noreply.github.com>
2022-12-08 07:57:49 -0300

    Add files via upload

---
+++ pryOpenBootCampN1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Ingresa tu usuario");
+string Nombre = Console.ReadLine();
+DateTime Hora = DateTime.Now;
+
+Console.WriteLine("Hola, " + Nombre + " Tu fecha y horario de ingreso es " + Hora);
